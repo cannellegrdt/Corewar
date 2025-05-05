851e6abe6b426fe7d4872d62f9d2697af8b7d667
Cannelle Gourdet <cannelle.gourdet@epitech.eu>
2025-05-05 17:45:31 +0200

    [ADD] process die once champion die

--- src/run_vm.c
+++ src/run_vm.c
@@ -119,10 +119,20 @@ void check_lives(vm_t *vm)
     process_t *current = vm->processes;
     process_t *prev = NULL;
     process_t *next;
+    bool champion_alive = false;
 
     while (current) {
         next = current->next;
-        if (current->alive && (vm->cycle_counter - current->last_live_cycle) > vm->cycle_to_die) {
+        champion_alive = false;
+        for (int i = 0; i < vm->champion_count; i++) {
+            if (vm->champions[i]->number == current->champion_number && 
+                vm->champions[i]->alive) {
+                champion_alive = true;
+                break;
+            }
+        }
+        if (!champion_alive ||
+        (current->alive && (vm->cycle_counter - current->last_live_cycle) > vm->cycle_to_die)) {
             if (prev)
                 prev->next = next;
             else
