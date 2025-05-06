c17e8e918ec2c1b39f20dd92298dc2513d78fcba
Cannelle Gourdet <cannelle.gourdet@epitech.eu>
2025-05-06 21:34:22 +0200

    add: debug function for -a
    Signed-off-by: Cannelle Gourdet <cannelle.gourdet@epitech.eu>

--- src/load_champion.c
+++ src/load_champion.c
@@ -53,6 +53,20 @@ static int load_single_champion(vm_t *vm, champion_t *champ, int index)
     return 0;
 }
 
+/*
+void debug_print_champion_addresses(vm_t *vm)
+{
+    printf("=== DEBUG: Champion load addresses ===\n");
+    for (int i = 0; i < vm->champion_count; i++) {
+        printf("Champion %d (%s): Address 0x%04X (decimal: %d)\n", 
+               vm->champions[i]->number,
+               vm->champions[i]->name, 
+               vm->champions[i]->load_address,
+               vm->champions[i]->load_address);
+    }
+    printf("================================\n");
+}
+*/
 int load_champions(vm_t *vm)
 {
     int result;
@@ -60,6 +74,7 @@ int load_champions(vm_t *vm)
     if (!(vm && vm->champions && vm->champion_count > 0))
         return error_msg("Error: invalid virtual machine state.\n", 84);
     calculate_load_addresses(vm);
+    //debug_print_champion_addresses(vm);
     for (int i = 0; i < vm->champion_count; i++) {
         result = load_single_champion(vm, vm->champions[i], i);
         if (result != 0)
