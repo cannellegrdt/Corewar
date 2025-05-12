90805b7d7fd13bbafa05946cee29a003413dff6b
Cannelle Gourdet <cannelle.gourdet@epitech.eu>
2025-05-12 15:44:23 +0200

    fix: coding-style
    Signed-off-by: Cannelle Gourdet <cannelle.gourdet@epitech.eu>

--- src/decode_args_with_coding_byte.c
+++ src/decode_args_with_coding_byte.c
@@ -50,8 +50,28 @@ static int read_indirect_arg(vm_t *vm, process_t *process, int i, int offset)
     return offset + IND_SIZE;
 }
 
-int decode_args_with_coding_byte(vm_t *vm, process_t *process,
-    const op_t *op, int offset)
+static bool is_valid_register(int reg_value)
+{
+    return reg_value >= 1 && reg_value <= REG_NUMBER;
+}
+
+static int process_single_argument(vm_t *vm, process_t *process,
+    int arg_index, int arg_type, int offset)
+{
+    process->current_op_arg_types[arg_index] = arg_type;
+    if (arg_type == T_REG) {
+        offset = read_register_arg(vm, process, arg_index, offset);
+        if (!is_valid_register(process->current_op_args[arg_index]))
+            return offset;
+    } else if (arg_type == T_DIR)
+        return read_direct_arg(vm, process, arg_index, offset);
+    if (arg_type == T_IND)
+        return read_indirect_arg(vm, process, arg_index, offset);
+    return offset;
+}
+
+int decode_args_with_coding_byte(vm_t *vm, process_t *process, const op_t *op,
+    int offset)
 {
     byte_t coding_byte = vm->memory[(process->pc + offset) % MEM_SIZE];
     int start_offset = offset;
@@ -59,20 +79,14 @@ int decode_args_with_coding_byte(vm_t *vm, process_t *process,
 
     offset++;
     for (int i = 0; i < op->nbr_args; i++) {
-        arg_type = convert_arg_code_to_type((coding_byte >> (6 - 2 * i))
-            & 0x3);
-        if (arg_type == 0 || !((arg_type & op->type[i]) != 0))
+        arg_type = convert_arg_code_to_type((coding_byte >> (6 - 2 * i)) &
+        0x3);
+        if (!(arg_type != 0 && ((arg_type & op->type[i]) != 0)))
             return start_offset + 1;
-        process->current_op_arg_types[i] = arg_type;
-        if (arg_type == T_REG) {
-            offset = read_register_arg(vm, process, i, offset);
-            if (!(process->current_op_args[i] >= 1 &&
-            process->current_op_args[i] <= REG_NUMBER))
-                return offset;
-        } else if (arg_type == T_DIR)
-            offset = read_direct_arg(vm, process, i, offset);
-        else if (arg_type == T_IND)
-            offset = read_indirect_arg(vm, process, i, offset);
+        offset = process_single_argument(vm, process, i, arg_type, offset);
+        if (arg_type == T_REG &&
+            !is_valid_register(process->current_op_args[i]))
+            return offset;
     }
     return offset;
 }
